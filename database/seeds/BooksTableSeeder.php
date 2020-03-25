7ed32d3e60f43323d9d180b2193ffd54742d42b3
manuelatrotta <manuelat88@hotmail.it>
2020-03-25 17:48:22 +0100

    correct

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -15,12 +15,12 @@ class BooksTableSeeder extends Seeder
       for ($i=0; $i < 10; $i++) {
         $newBook = new Book;
         $newBook->title = $faker->word;
-        $newShoe->author = $faker->name;
-        $newShoe->genre = $faker->name;
-        $newShoe->description = $faker->paragraph($nbSentences = 3, $variableNbSentences = true);
-        $newShoe->price =
+        $newBook->author = $faker->name;
+        $newBook->genre = $faker->name;
+        $newBook->description = $faker->paragraph($nbSentences = 3, $variableNbSentences = true);
+        $newBook->price =
         $faker->randomFloat($nbMaxDecimals = 2, $min = 30, $max = 9999);
-        $newShoe->date_production = '2019-01-01';
-        $newShoe->save();
+        $newBook->date_production = '2019-01-01';
+        $newBook->save();
     }
 }
