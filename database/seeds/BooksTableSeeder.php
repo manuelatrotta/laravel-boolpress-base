3e54c6248ee94047175005994215981e6d4fff6f
manuelatrotta <manuelat88@hotmail.it>
2020-03-25 17:44:55 +0100

    inserimento faker

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -9,8 +9,17 @@ class BooksTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+      for ($i=0; $i < 10; $i++) {
+        $newBook = new Book;
+        $newBook->title = $faker->word;
+        $newShoe->author = $faker->name;
+        $newShoe->genre = $faker->name;
+        $newShoe->description = $faker->paragraph($nbSentences = 3, $variableNbSentences = true);
+        $newShoe->price =
+        $faker->randomFloat($nbMaxDecimals = 2, $min = 30, $max = 9999);
+        $newShoe->date_production = '2019-01-01';
+        $newShoe->save();
     }
 }
