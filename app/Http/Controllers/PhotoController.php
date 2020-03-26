e5141dad1567d9e46e7527c225f42caac14549dc
manuelatrotta <manuelat88@hotmail.it>
2020-03-26 18:05:32 +0100

    creazione route

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Photo;
 
 class PhotoController extends Controller
 {
@@ -13,7 +14,10 @@ class PhotoController extends Controller
      */
     public function index()
     {
-        //
+      $photos = Photo::all();
+
+      return view('photo.index', compact('photos'));
+      
     }
 
     /**
